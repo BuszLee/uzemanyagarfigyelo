2e6e29e43d9dc0772f5b06779c292d233e65a0d1
BuszLee <acer2300@gmail.com>
2026-04-23 12:42:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request
-import random
+import requests
 
 app = Flask(__name__)
 
@@ -10,46 +10,50 @@ def home():
 @app.route("/stations")
 def stations():
 
-    lat = request.args.get("lat", 47.4979, type=float)
-    lon = request.args.get("lon", 19.0402, type=float)
+    lat = request.args.get("lat", type=float)
+    lon = request.args.get("lon", type=float)
 
-    stations = [
-        {
-            "name": "OMV Budapest, Váci út",
-            "lat": lat + 0.010,
-            "lon": lon + 0.005,
+    if lat is None or lon is None:
+        lat = 47.4979
+        lon = 19.0402
+
+    query = f"""
+    [out:json];
+    (
+      node["amenity"="fuel"](around:5000,{lat},{lon});
+      way["amenity"="fuel"](around:5000,{lat},{lon});
+      relation["amenity"="fuel"](around:5000,{lat},{lon});
+    );
+    out center;
+    """
+
+    url = "https://overpass-api.de/api/interpreter"
+
+    r = requests.get(url, params={"data": query}, timeout=20)
+    data = r.json()
+
+    stations = []
+
+    for item in data["elements"]:
+
+        tags = item.get("tags", {})
+
+        name = tags.get("name", "Benzinkút")
+
+        if "lat" in item:
+            station_lat = item["lat"]
+            station_lon = item["lon"]
+        else:
+            station_lat = item["center"]["lat"]
+            station_lon = item["center"]["lon"]
+
+        stations.append({
+            "name": name,
+            "lat": station_lat,
+            "lon": station_lon,
             "fuelType": "95",
-            "price": 617
-        },
-        {
-            "name": "Shell Budapest, Hungária körút",
-            "lat": lat - 0.008,
-            "lon": lon + 0.012,
-            "fuelType": "95",
-            "price": 621
-        },
-        {
-            "name": "MOL Budapest, Üllői út",
-            "lat": lat + 0.006,
-            "lon": lon - 0.010,
-            "fuelType": "95",
-            "price": 612
-        },
-        {
-            "name": "Auchan Benzinkút",
-            "lat": lat - 0.012,
-            "lon": lon - 0.008,
-            "fuelType": "95",
-            "price": 606
-        },
-        {
-            "name": "AVIA Budapest",
-            "lat": lat + 0.015,
-            "lon": lon + 0.002,
-            "fuelType": "95",
-            "price": 609
-        }
-    ]
+            "price": 615
+        })
 
     return jsonify({
         "stations": stations
